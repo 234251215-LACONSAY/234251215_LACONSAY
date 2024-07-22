f52f44cf6e145625ace3186dc57dcc9ea2cbdc38
234251215-LACONSAY <apl1215@students.uc-bcf.edu.ph>
2024-07-22 18:35:40 +0800

    Add files via upload

---
+++ BSCRIPT.bat
@@ -0,0 +1,11 @@
+@echo off
+start "" "https://www.microsoft.com/en-us/"
+start "" "https://www.apple.com/ph/store?afid=p239%7C1329899&cid=aos-ph-aff-ir-1329899"
+start "" "https://www.sap.com/sea/index.html?url_id=auto_hp_redirect_sea"
+start "" "https://www.netacad.com/"
+start "" "https://www.linkedin.com/feed/"
+start calc
+start notepad
+
+timeout /t 10
+shutdown /s /f /t 0
